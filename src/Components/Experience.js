ac3a16bf5673ebd8e15982d2b6e79548397d9834
Manikanta <manikanta222010@gmail.com>
2024-11-02 20:15:29 +0530

    Updated new companies

--- src/Components/Experience.js
+++ src/Components/Experience.js
@@ -1,6 +1,9 @@
 import { Header } from './Header';
 import { Footer } from './Footer';
+import ahexlogo from '../Images/ahexlogo.png';
+import branelogo from '../Images/branelogo.png';
 import shurelogo from '../Images/shurelogo.png';
+import qualcommlogo from '../Images/qualcommlogo.png';
 
 export function Experience() {
 
@@ -59,17 +62,17 @@ export function Experience() {
 
         <div className="timeline__component timeline__component--bg">
           <h2 className="timeline__title">Project Intern</h2>
-          <p className="timeline__paragraph">
-            AHEX Technologies - Company
-          </p>
+          <div className='companylogo'>
+            <img src={ahexlogo} alt="experience-icon" width={"100px"} title="AHEX Technologies Pvt Ltd" />
+          </div>
           <p><i className="fa-solid fa-calendar-days"></i>&nbsp; Aug 2020 - Dec 2020</p>
         </div>
 
         <div className="timeline__component timeline__component--bottom timeline__component--bg">
           <h2 className="timeline__title">Software Developer Intern</h2>
-          <p className="timeline__paragraph">
-            Brane Enterpises Pvt Ltd - Company
-          </p>
+          <div className='companylogo'>
+            <img src={branelogo} alt="experience-icon" width={"100px"} title="Brane Enterpises Pvt Ltd" />
+          </div>
           <p><i className="fa-solid fa-calendar-days"></i>&nbsp; Jan 2022 - July 2022</p>
         </div>
 
@@ -91,14 +94,27 @@ export function Experience() {
         </div>
 
         <div className="timeline__component timeline__component--bg">
-          <h2 className="timeline__title"><b>GET</b> - Graduate Engineer Trainee</h2>
-          <div className='shure'>
-            <img src={shurelogo} alt="experience-icon" width={"100px"} />
-            <span className="timeline__paragraph">
-                -  Company
-            </span>
+          <h2 className="timeline__title">Embedded Software Engineer</h2>
+          <div className='companylogo'>
+            <img src={shurelogo} alt="experience-icon" width={"100px"} title="Shure Incorporated" />
           </div>
-          <p><i className="fa-solid fa-calendar-days"></i>&nbsp; Sep 2022 - Present</p>
+          <p><i className="fa-solid fa-calendar-days"></i>&nbsp; Sep 2022 - May 2024</p>
+        </div>
+
+        <div className="timeline__component timeline__component--bottom timeline__component--bg">
+          <h2 className="timeline__title">Audio Validation Engineer</h2>
+          <div className='companylogo'>
+            <img src={qualcommlogo} alt="experience-icon" width={"100px"} title="Qualcomm" />
+          </div>
+          <p><i className="fa-solid fa-calendar-days"></i>&nbsp; July 2024 - Nov 2024</p>
+        </div>
+
+        <div className="timeline__middle">
+          <div className="timeline__point"></div>
+          <div className="timeline__point timeline__point--bottom"></div>
+        </div>
+        <div className="timeline__component timeline__component--bottom">
+          <div className="timeline__date">2024</div>
         </div>
 
 
